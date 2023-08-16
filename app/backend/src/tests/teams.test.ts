0a431fe8af3e70e60d183b4148724de2172f8b2b
MarcoAureliojunqueira <marcoaureliojunqueira8@gmail.com>
2023-08-16 01:40:10 -0300

    requisito 6

--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -15,10 +15,18 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('Seu teste', () => {
-  it('should return all teams', async function() {
+  it('get Teams', async function() {
     sinon.stub(ModelTeams ,'findAll').resolves(Teams as ModelTeams[]);
 
     const { status, body } = await chai.request(app).get('/teams');
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(Teams);
+  });
+
+  it(' getById Teams', async function() {
+    sinon.stub(ModelTeams ,'findByPk').resolves(Teams as any);
+
+    const { status, body } = await chai.request(app).get('/teams/1');
     expect(status).to.equal(200);
     expect(body).to.deep.equal(Teams);
   });
